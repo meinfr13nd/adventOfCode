21375319dd857980943f96426b384c599e0943cc
meinfr13nd <sarahsamora@mail.com>
2018-12-18 21:26:58 -0800

    finished day 6!!

--- day6Part1.java
+++ day6Part1.java
@@ -7,39 +7,60 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.awt.Point;
 
-
 public class day6Part1 extends AdventOfCode {
 //3223
+
+ArrayList<Point> pts = new ArrayList<Point>();
+int minX = Integer.MAX_VALUE;
+int maxX = Integer.MIN_VALUE;
+int minY = Integer.MAX_VALUE;
+int maxY = Integer.MIN_VALUE;
+HashMap<Point, Integer> closestTo = new HashMap<Point, Integer>();
 
 public day6Part1(String fileName) {
         super(fileName);
 }
 
-private int minX = Integer.MAX_VALUE;
-private int maxX = Integer.MIN_VALUE;
-private int minY = Integer.MAX_VALUE;
-private int maxY = Integer.MIN_VALUE;
-private Color edge = new Color("edge", 0);
-private HashMap<Point, Color> pointToColor = new HashMap<Point, Color>();
-private ArrayList<Color> colors = new ArrayList<Color>();
-private HashSet<Point> processed = new HashSet<Point>();
-
 protected String getAnswer() {
         exploreMap();
-        int max = Integer.MIN_VALUE;
-        for (Color c: colors) {
-                max = Math.max(c.getCount(), max);
+        removeInfinite();
+        int maxClosest = Integer.MIN_VALUE;
+        for (Point pt : closestTo.keySet()) {
+                if (closestTo.get(pt) > maxClosest) {
+                        maxClosest = closestTo.get(pt);
+                }
         }
-        return ((Integer)(max)).toString();
+        return ((Integer)maxClosest).toString();
+}
+
+private static Point nearestPoint(Point p, ArrayList<Point> pts) {
+        Point minPoint = new Point(0, 0);
+        int numAtDist = 0;
+        int minDist = Integer.MAX_VALUE;
+        for (Point pt: pts) {
+                int dist = Math.abs(p.x - pt.x) + Math.abs(p.y - pt.y);
+                if (dist < minDist) {
+                        minDist = dist;
+                        numAtDist = 1;
+                        minPoint = pt;
+                } else if (dist == minDist) {
+                        numAtDist++;
+                }
+        }
+        if (numAtDist > 1) {
+                return null;
+        } else {
+                return minPoint;
+        }
 }
 
 protected Object processLine(String line) {
         String numbers[];
-        numbers = line.split("\\s+");
-        int x = Integer.parseInt(numbers[0].substring(0, numbers[0].length()-1));
-        int y = Integer.parseInt(numbers[1]);
+        numbers = line.split(",");
+        int x = Integer.parseInt(numbers[0].trim());
+        int y = Integer.parseInt(numbers[1].trim());
         Point p = new Point(x, y);
-        processed.add(p);
+        pts.add(p);
         if (y > maxY) {
                 maxY = y;
         } else if (y < minY) {
@@ -50,39 +71,36 @@ protected Object processLine(String line) {
         } else if (x < minX) {
                 minX = x;
         }
-        Color clr = new Color(line, 1);
-        colors.add(clr);
-        pointToColor.put(p, clr);
-        return p;
+        closestTo.put(p, 0);
+        return null;
 }
 
 private void exploreMap() {
-        while (this.lineOutput.size() != 0) {
-                Point current = (Point)this.lineOutput.get(0);
-                Color currentClr = pointToColor.get(current);
-                this.lineOutput.remove(0);
-                for (int i = -1; i <= 1; i++) {
-                        for (int j = -1; j <= 1; j++) {
-                                if (!currentClr.equals(edge)) {
-                                        Point p = new Point(current.x + i, current.y + j);
-                                        if (pointToColor.get(p) != null && !currentClr.equals(pointToColor.get(p))) {
-                                                pointToColor.get(p).decrementCount();
-                                                pointToColor.put(p, edge);
-
-                                        } else if (p.x <= minX || p.x >= maxX || p.y <= minY || p.y >= maxY) {
-                                                currentClr.markEdge();
-                                                pointToColor.put(p, currentClr);
-                                        } else if (!processed.contains(p)) {
-                                                pointToColor.put(p, currentClr);
-                                                this.lineOutput.add(p);
-                                                currentClr.incrementCount();
-                                                processed.add(p);
-                                        }
-                                }
-
+        for (int i = minX; i <= maxX; i++) {
+                for (int j = minY; j <= maxY; j++) {
+                        Point p = new Point(i, j);
+                        Point closest = nearestPoint(p, pts);
+                        if (closest != null) {
+                                closestTo.put(closest, closestTo.get(closest) + 1);
                         }
                 }
         }
 }
+
+private void removeInfinite() {
+        HashSet<Point> toRemove = new HashSet<Point>();
+        for (int i = minX; i <= maxX; i++) {
+                toRemove.add(nearestPoint(new Point(i, minY), pts));
+                toRemove.add(nearestPoint(new Point(i, maxY), pts));
+        }
+        for (int j = minY; j <= maxY; j++) {
+                toRemove.add(nearestPoint(new Point(minX, j), pts));
+                toRemove.add(nearestPoint(new Point(maxX, j), pts));
+        }
+        for (Point p : toRemove) {
+                closestTo.remove(p);
+        }
+}
+
 
 }
