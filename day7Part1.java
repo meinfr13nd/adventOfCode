13fe7541d2a4a31935b62809262ee852178f3823
meinfr13nd <sarahsamora@mail.com>
2018-12-21 15:06:00 -0800

    working day 7 part 1

--- day7Part1.java
+++ day7Part1.java
@@ -1,56 +1,115 @@
+//largely based on code from harto on this stack overflow question https://stackoverflow.com/questions/2739392/sample-directed-graph-and-topological-sort-code
+
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class day7Part1 extends AdventOfCode {
 
-HashMap<String, HashSet<String> > stringToAdjacent = new HashMap<String, HashSet<String> >();
+private final HashMap<String, Node> keyToNode = new HashMap<String, Node>();
+
+protected String getAnswer() {
+        return drawGraph();
+}
 
 public day7Part1(String fileName) {
         super(fileName);
 }
 
-protected String getAnswer() {
-        drawGraph();
-        return getOrder();
-}
-
 protected Object processLine(String line) {
         String one = line.charAt(5) + "";
         String two = line.charAt(36) + "";
-        String[] letters = {one, two};
-        return letters;
+        Node node1 = new Node(one);
+        Node node2 = new Node(two);
+        if (keyToNode.containsKey(one)) {
+                node1 = keyToNode.get(one);
+        } else {
+                keyToNode.put(one, node1);
+        }
+        if (keyToNode.containsKey(two)) {
+                node2 = keyToNode.get(two);
+        } else {
+                keyToNode.put(two, node2);
+        }
+        node1.addEdge(node2);
+        return null;
 }
 
-private void drawGraph() {
-        for (int i = 0; i < this.lineOutput.size(); i++) {
-                String[] letters = (String[])this.lineOutput.get(i);
-                if (stringToAdjacent.containsKey(letters[0])) {
-                        stringToAdjacent.get(letters[0]).add(letters[1]);
-                } else {
-                        HashSet<String> adjacency = new HashSet<String>();
-                        adjacency.add(letters[1]);
-                        stringToAdjacent.put(letters[0], adjacency);
+private class Node implements Comparable<Node>{
+private final String key;
+private final HashSet<Edge> inEdges;
+private final HashSet<Edge> outEdges;
+
+private Node(String key) {
+        this.key = key;
+        inEdges = new HashSet<Edge>();
+        outEdges = new HashSet<Edge>();
+}
+
+@Override
+public int compareTo(Node n) {
+        return this.key.compareTo(n.key);
+}
+
+public Node addEdge(Node node){
+        Edge e = new Edge(this, node);
+        outEdges.add(e);
+        node.inEdges.add(e);
+        return this;
+}
+public String getKey() {
+        return key;
+}
+}
+
+private class Edge {
+private final Node from;
+private final Node to;
+private Edge(Node from, Node to) {
+        this.from = from;
+        this.to = to;
+}
+@Override
+public boolean equals(Object obj) {
+        Edge e = (Edge)obj;
+        return e.from == from && e.to == to;
+}
+}
+
+private String drawGraph() {
+        ArrayList<Node> sortedElements = new ArrayList<Node>(); // Empty list that will contain the sorted elements
+        HashSet<Node> noIncomingEdges = new HashSet<Node>(); // Set of all nodes with no incoming edges
+        for(String s : keyToNode.keySet()) {
+                Node n = keyToNode.get(s);
+                if(n.inEdges.size() == 0) {
+                        noIncomingEdges.add(n);
                 }
         }
-}
+        //while S is non-empty do
+        while(!noIncomingEdges.isEmpty()) {
+                ArrayList<Node> sortedNexts = new ArrayList<Node>(new TreeSet<Node>(noIncomingEdges));
+                Node n = sortedNexts.get(0);
+                noIncomingEdges.remove(n);
+                sortedElements.add(n);
+                for(Iterator<Edge> it = n.outEdges.iterator(); it.hasNext();) { //for each node m with an edge e from n to m do
+                        //remove edge e from the graph
+                        Edge e = it.next(); //remove edge e from the graph
+                        Node m = e.to;
+                        it.remove();        //Remove edge from n
+                        m.inEdges.remove(e);        //Remove edge from m
 
-private String getOrder() {
-        System.out.println(stringToAdjacent);
-        int numMax = 0;
-        int max = 0;
-        ArrayList<String> possibleNext;
-        for (String s: stringToAdjacent.keySet()) {
-                System.out.println(stringToAdjacent.get(s));
-                if (max < stringToAdjacent.get(s).size()) {
-                        max = stringToAdjacent.get(s).size();
-                        numMax = 1;
-                } else if (max == stringToAdjacent.get(s).size()) {
-                        numMax += 1;
+                        if(m.inEdges.isEmpty()) { //if m has no other incoming edges then insert m into S
+                                noIncomingEdges.add(m);
+                        }
                 }
         }
-        System.out.println(numMax + "  " + max);
-        return "";
+        String order = "";
+        for (Node n : sortedElements) {
+                order += n.getKey();
+        }
+        return order;
 }
-
 }
