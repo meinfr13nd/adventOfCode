86e05c1159c92ab82e23399f55237e781be3810d
meinfr13nd <sarahsamora@mail.com>
2018-12-21 13:03:55 -0800

    shells for day 7 part 2, day 8 part 1, day 8 part 2

---
+++ day8Part1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class day8Part1 extends AdventOfCode {
+
+public day8Part1(String fileName) {
+        super(fileName);
+}
+
+protected String getAnswer() {
+        return "";
+}
+
+protected Object processLine(String line) {
+        return null;
+}
+
+}
